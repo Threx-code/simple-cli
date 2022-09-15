2d9daa2174932255740b47e4d7916fa86131ea87
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2022-09-15 09:21:38 +0100

    Updating the files

--- System/Views/search.php
+++ System/Views/search.php
@@ -32,31 +32,29 @@ require_once "nav_bar.php";
                         <?php
                         if (!empty(json_decode($data, true))) {
                             foreach ($data as $val) { ?>
-                             <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4" style="border: 1px solid #eee; padding: 20px">
-                                <strong>County: <?php echo $val->county; ?></strong>
-                                <small>Country: <?php echo $val->country; ?></small>
-                                <small>Town: <?php echo $val->town; ?></small>
-                                <small><p>Address:<?php echo $val->address; ?></p></small>
-                                <small>Long: <?php echo $val->latitude; ?> Lat: <?php echo $val->longitude; ?></small>
-                                <a href="<?php echo $val->image; ?>" class="item-wrap" data-fancybox="gal">
-                                    <span class="icon-search2"></span>
-                                    <img class="img-fluid" src="<?php echo $val->image; ?>">
-                                     <img src="<?php echo $val->thumbnail; ?>" style="z-index: 10; position: relative; margin-top: -100px; margin-left:240px;">
-                                </a>
-                                 <small>No of Bedrooms: <?php echo $val->num_bedrooms; ?> -> No of Bathrooms: <?php echo $val->num_bathrooms; ?></small>
-                                <p style="font-size: 12px;"><?php echo $val->description; ?></p>
-
-                                    <p>Price: <?php echo $val->price; ?></p>
-                                    <?php $pro_type = json_decode($val->property_type, true); ?>
-                                    <small><strong>Type:</strong> <?php echo($pro_type['type']); ?><br>
-                                    <strong>Description:</strong> <?php echo($pro_type['description']); ?></small><br>
+                                <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4 card" style="border: 1px solid #eee; padding: 20px">
+                                    <a href="<?php echo $val->image; ?>" class="item-wrap" data-fancybox="gal">
+                                        <picture class="card-img-top">
+                                            <source srcset="<?php echo $val->image; ?>" type="image/svg+xml">
+                                            <img src="<?php echo $val->image; ?>" class="img-fluid img-thumbnail" alt="...">
+                                        </picture>
+                                    </a>
+                                    <strong>County: <?php echo $val->county; ?></strong>
+                                    <small>Country: <?php echo $val->country; ?></small>
+                                    <small>Town: <?php echo $val->town; ?></small>
+                                    <small>Address:<?php echo $val->address; ?></small>
+                                    <small><strong>Long:</strong> <?php echo $val->latitude; ?> <strong>Lat:</strong> <?php echo $val->longitude; ?></small>
+                                    <small>No of Bedrooms: <?php echo $val->num_bedrooms; ?> -> No of Bathrooms: <?php echo $val->num_bathrooms; ?></small>
                                     <small><strong>Available for:</strong> <?php echo $val->type; ?></small>
                                     <small><strong>Date Posted:</strong> <?php echo $val->created_at; ?></small>
-                                    <div class="form-group mt-4">
-                                        <a href="edit?item_no=<?php echo $val->id; ?>&token=<?php echo $crfToken; ?>" class="item-wrap btn btn-primary mb-4" data-fancybox="gal">Edit</a>
-                                    </div>
-                             </div>
+                                    <small>Price: <?php echo $val->price; ?></small>
+                                    <?php $pro_type = json_decode($val->property_type, true); ?>
+                                    <small><strong>Type:</strong> <?php echo($pro_type['type']); ?><br>
+                                        <strong>Description:</strong> <?php echo($pro_type['description']); ?>
+                                    </small><br>
 
+                                    <a href="edit?item_no=<?php echo $val->id; ?>&token=<?php echo $crfToken; ?>" class="item-wrap btn btn-primary mb-4" data-fancybox="gal">Edit</a>
+                                </div>
                             <?php
                             }
                         } else {
